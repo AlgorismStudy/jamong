f8526c2c8d2e311d58e28a0f7275e518b2296f45
eunsun <nammugmuhg@gmail.com>
2022-01-16 20:16:46 +0900

    2022.1.16

--- baek/DFS_BFS/1697.js
+++ baek/DFS_BFS/1697.js
@@ -1,34 +1,30 @@
 const fs = require("fs");
-const [N,K] = fs
+const [N, K] = fs
   .readFileSync("1697.txt")
   .toString()
   .trim()
   .split(" ")
-    .map((x) => +x);
-console.log(N,K)
-const visited = new Array(100100).fill(false);
-const queue = []
-let v = 0;
+  .map((x) => +x);
+
+const visited = new Array(100000).fill(false);
+const queue = [];
+
 //수빈은 앞 뒤로 X+1 혹은 X-1로 이동. 순간이동시 2*X의 위치로 이동
 //수빈이가 동생 찾는 가장 빠른 시간 구하기
 
 visited[N] = true;
-queue.push([N,0]);
+queue.push([N, 0]);
 
-while (queue.length) { 
-    const [cur, time] = queue.shift();
-    if (cur == K) {
-        console.log(time);
-        break;
+while (queue.length) {
+  const [cur, time] = queue.shift();
+  if (cur == K) {
+    console.log(time);
+    break;
+  }
+  for (i of [cur - 1, cur + 1, cur * 2]) {
+    if (!visited[i] && i >= 0 && i <= 100000) {
+      queue.push([i, time + 1]);
+      visited[i] = true;
     }
-    for (i of[cur-1,cur+1,cur*2]) {
-        if (!visited[i] && i >=0 && i<=10000) {
-            queue.push([i,time+1]);
-            visited[i] = true;
-        }
-    }
-    
-        
-    }
-
-
+  }
+}
---
+++ baek/DP/1463.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const N =
+    Number(fs.readFileSync("1463.txt").toString().trim());
+console.log(N);
+//정수 N이 주어졌을 때, 3이나 2로 나누어떨어지면 3이나 2로 나누고, 1을 빼는 연산
+//위 연산 3개를 적절히 사용하여 1 만듦.
+
+const d = new Array(N + 1).fill(0);
+
+for (i = 2; i <= N; i++) {
+  d[i] = d[i - 1] + 1;
+  //그 전 수에 1을 더하는값으로 초기화(그 전 수 기준으로는 1을 더하는게 가장 최소)
+  if (i % 2 == 0) {
+    d[i] = Math.min(d[i], d[i / 2] + 1);
+  }
+  if (i % 3 == 0) {
+    d[i] = Math.min(d[i], d[i / 3] + 1);
+  }
+}
+
+console.log(d[N]);
