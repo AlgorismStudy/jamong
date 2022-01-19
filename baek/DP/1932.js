4ca8f77620ca03f5b8d7037d1fd646ef43fd36c7
eunsun <nammugmuhg@gmail.com>
2022-01-20 03:13:45 +0900

    2022.1.20

--- baek/DP/1932.js
+++ baek/DP/1932.js
@@ -9,6 +9,7 @@ for (i = 1; i < n; i++) {
     if (j === 0) prev = A[i - 1][0];//가장왼쪽에있으면 부모의 가장 왼쪽 값
     else if (j === i) prev = A[i - 1][j - 1];//가장오른쪽값은 부모값
     else prev = Math.max(A[i - 1][j - 1], A[i - 1][j]);//왼쪽값과 현재 인덱스값 자리의 큰 부모값
+     A[i][j] += prev;
   }
 }
 console.log(Math.max(...A[n-1]))
