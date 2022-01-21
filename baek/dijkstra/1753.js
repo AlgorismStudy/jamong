32c0db784b83c92e2073a410b143a361dfb96353
eunsun <nammugmuhg@gmail.com>
2022-01-21 23:26:30 +0900

    2022.1.21

--- baek/dijkstra/1753.js
+++ baek/dijkstra/1753.js
@@ -8,3 +8,62 @@
 // 출력
 // 첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다. 시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
 
+
+
+
+const fs = require("fs");
+const input = fs.readFileSync("/input/1753.txt").toString().trim().split("\n");
+
+const [v, e] = input[0].split(" ").map((x) => +x);
+const start = Number(input[1]);
+const graph = input.slice(2).map((x) => x.split(" ").map((x) => +x));
+
+
+
+const dijkstra = (n, adj) => {
+  const dist = Array(n + 1).fill(false);
+  const queue = [];
+
+  queue.push({ to: 1, cost: 0 });
+  dist[1] = 0;
+
+  while (queue.length !== 0) {
+    let { to, cost } = queue.shift();
+
+    adj[to].map((nextNode) => {
+      const nextCost = cost + 1;
+      if (dist[nextNode] === false) {
+        dist[nextNode] = nextCost;
+        queue.push({ to: nextNode, cost: nextCost });
+      }
+    });
+  }
+
+  const max = Math.max(...dist);
+  return dist.filter((num) => {
+    return num === max;
+  }).length;
+};
+
+function solution(n, edge) {
+  let answer = 0;
+  let adj = Array.from({ length: n + 1 }, () => []);
+
+  // 인접 리스트 생성
+  edge.forEach((route) => {
+    adj[route[0]].push(route[1]);
+    adj[route[1]].push(route[0]);
+  });
+
+  // 다익스트라
+  answer = dijkstra(n, adj);
+  return answer;
+}
+
+
+dijkstra(graph);
+for (i = 1; i <= v; i++) {
+  console.log(distance[i]);
+}
+
+
