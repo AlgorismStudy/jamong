7a16ddea8f6e90c97b58255d033409c0122d1deb
eunsun <nammugmuhg@gmail.com>
2022-04-28 01:12:40 +0900

    1058

--- baek/warshall/1058.js
+++ baek/warshall/1058.js
@@ -1,18 +1,35 @@
 const fs = require("fs");
 const input = fs.readFileSync("input/1058.txt").toString().trim().split("\n");
 
-
 const N = input[0];
 const relation = input.slice(1);
-console.log(N, relation)
-
-
-for (let i = 0; i < relation.length; i++){
-    for (let j = 0; j < relation.length; i++){
-        dfs(relation[i][j],i,j, 0);
+let TFriend = new Array(relation.length).fill(
+  new Array(relation.length).fill(0)
+);
+console.log(TFriend);
+for (let i = 0; i < relation.length; i++) {
+  for (let j = i; j < relation.length; j++) {
+    console.log(relation[i][j]);
+    if (relation[i][j] == "Y" && i != j) {
+      //친구의 관계속으로 들어가기
+      dfs(i, j, 1);
+    } else {
+      continue;
     }
+  }
 }
-function dfs(friend, depth) {
+function dfs(origin, friend, depth) {
+  if (depth == 3) return;
+  for (let i = 0; i < relation.length; i++) {
+    if (relation[friend][i] == "Y" && i != origin) {
+      TFriend[origin][] += 1;
+      dfs(origin, i, depth + 1);
+    } else {
+      continue;
+    }
+  }
+}
 
-    
-}
+console.log(TFriend);
+
+//fail
