926c4627830e977e515aa9e641e2ab58f68e7c13
eunsun <nammugmuhg@gmail.com>
2022-04-29 01:37:59 +0900

    2차원 배열 만들때 주의

--- baek/warshall/1058.js
+++ baek/warshall/1058.js
@@ -1,35 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("input/1058.txt").toString().trim().split("\n");
+const N = +input[0];
+const graph = input.slice(1).map((v) => v.split(""));
 
-const N = input[0];
-const relation = input.slice(1);
-let TFriend = new Array(relation.length).fill(
-  new Array(relation.length).fill(0)
-);
-console.log(TFriend);
-for (let i = 0; i < relation.length; i++) {
-  for (let j = i; j < relation.length; j++) {
-    console.log(relation[i][j]);
-    if (relation[i][j] == "Y" && i != j) {
-      //친구의 관계속으로 들어가기
-      dfs(i, j, 1);
-    } else {
-      continue;
-    }
-  }
-}
-function dfs(origin, friend, depth) {
-  if (depth == 3) return;
-  for (let i = 0; i < relation.length; i++) {
-    if (relation[friend][i] == "Y" && i != origin) {
-      TFriend[origin][] += 1;
-      dfs(origin, i, depth + 1);
-    } else {
-      continue;
+let visited = Array.from(Array(N), () => Array(N).fill(0));
+
+for (k = 0; k < N; k++) {
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      if (i === j) {
+        visited[i][j] = 0;
+        continue;
+      }
+      if (graph[i][j] === "Y") {
+        visited[i][j] = 1;
+      } else if (graph[i][k] === "Y" && graph[k][j] === "Y") {
+        visited[i][j] = 1;
+      }
     }
   }
 }
 
-console.log(TFriend);
+let result = 0;
+for (i = 0; i < N; i++) {
+  let sumV = visited[i].reduce((a, c) => a + c);
+  result = Math.max(result, sumV);
+}
 
-//fail
+console.log(result);
