28bbb2805516d12b2aad26450b0636b6d30df352
eunsun <nammugmuhg@gmail.com>
2022-04-30 23:19:30 +0900

    12973

---
+++ baek/implementation/12973.js
@@ -0,0 +1,23 @@
+function solution(s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr.push(s[i]);
+    if (arr[arr.length - 1] === arr[arr.length - 2]) {
+      arr.pop();
+      arr.pop();
+    }
+  }
+  return arr.join("") === "" ? 1 : 0;
+}
+
+
+function solution(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.length || stack[stack.length - 1] !== s[i]) stack.push(s[i]);
+    else stack.pop();
+  }
+
+  return stack.length ? 0 : 1;
+}
